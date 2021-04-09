bad37226b7df5ee695c4784e98d3d7a7c0e9786c
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-04-09 12:03:33 -0700

    set up UserSchema

---
+++ models/Users.js
@@ -0,0 +1,47 @@
+const { Schema, model } = reqire("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: "A username is required.",
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid email address."],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Friend",
+    },
+  ],
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false //not sure if i need htis ornot 
+});
+
+// UserSchema.virtual('friendCount').get(function() {
+//     return this.friends.reduce(total, friend) => total + fri
+// }
+
+//get total count of friends
+
+
+//create the user model using the UserSchema
+const User = model('User', UserSchema);
+
+//export the User model
+module.exports = User;
